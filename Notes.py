418bb7f0a948b6765d37fe585471af432cd3c973
aneroid13 <aneroid@inbox.ru>
2020-02-29 03:41:14 +0700

    Button events, title focus, title text

--- Notes.py
+++ Notes.py
@@ -1,5 +1,7 @@
 import docutils
 import time, uuid
+from inspect import getmembers as gm
+from pprint import pprint as pp
 from kivy.app import App
 from kivy.uix.togglebutton import ToggleButton as button
 from kivy.uix.textinput import TextInput as ti
@@ -12,41 +14,43 @@ from kivy.event import EventDispatcher
 
 class Note:
     def __init__(self):
-        self._n_id = uuid.uuid4()
-        self._n_create_time = time.time()
-        self._n_update_time = time.time()
-        self._n_title = ""
+        self.uid = uuid.uuid4()
+        self.create_time = time.time()
+        self.update_time = time.time()
+        self.title = ""
+        self.tags = []
+        self.bookmarked = False
+        self.trashed = False
 
     def get_id(self):
-        return self._n_id
+        return self.uid
 
-    def set_title(self, name):
-        self._n_title = name
+    # def create(self):
+    #     pass
+    #
+    # def save(self):
+    #     pass
 
-    def create(self):
+
+class NoteBank:
+    def __init__(self):
+        self.bank = []
+
+    def add_note(self, note):
+        self.bank.append(note)
+        return note.uid
+
+    def update_note(self):
         pass
-
-    def save(self):
-        pass
-
-    def bookmark(self):
-        pass
-
-    def tag(self):
-        pass
-
-    def trash(self):
-        pass
-
-    def delete(self):
-        pass
-
 
 class NotesApp(App):
     title = "Notes"
 
     def __init__(self):
         super().__init__()
+        self.bank = NoteBank()
+        self.current_note = None
+        self.current_note_button = None
 
     def create_note(self):
         note = Note()
@@ -55,8 +59,38 @@ class NotesApp(App):
         note_butt.group = "Notes"
         self.root.ids.note_bar.add_widget(note_butt)
 
+    def find_current_button(self):
+        for child in self.root.ids.note_bar.children:
+            if str(child.id) == str(self.current_note.uid):
+                self.current_note_button = child
+
+    def button_selection(self, instance, pos):
+        if pos is 'normal' and \
+                   str(instance.id) == str(self.current_note_button.id):
+            self.current_note_button = None
+            self.current_note = None
+
+        if pos is 'down':
+            self.current_note_button = instance
+            #TODO: get note
+
+    def create_new_note(self):
+        if self.root.ids.title.text == "" and not self.current_note:
+            self.current_note = Note()
+
+            note_butt = button()
+            note_butt.id = str(self.current_note.uid)
+            note_butt.state = 'down'
+            note_butt.group = "Notes"
+            note_butt.bind(state=self.button_selection)
+            self.root.ids.note_bar.add_widget(note_butt)
+            self.find_current_button()
+
     def title_entered(self):
-        pass
+        if self.root.ids.title.text:
+            self.current_note_button.text = self.root.ids.title.text
+
+
     # Event bind example
     # def on_start(self):
     #     self.root.ids['user_input'].bind(on_text_validate=self.enter_message)
