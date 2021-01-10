fda6a3bd42bf534ebf05fd940f13a8c56ad6e842
aneroid13 <aneroid@inbox.ru>
2021-01-11 02:22:16 +0700

    shelve backend

--- modules/shelf.py
+++ modules/shelf.py
@@ -13,10 +13,12 @@ class KnotsStore:
         self.check()
 
     def check(self): pass
+
     def save_info(self, store):
         shelf = shelve.open(self.shelf_file.as_posix())
         shelf['info'] = store
         shelf.close()
+
     def save_text(self, store):
         shelf = shelve.open(self.shelf_file.as_posix())
         if 'text' in shelf:
@@ -24,26 +26,31 @@ class KnotsStore:
         else:
             shelf['text'] = store
         shelf.close()
+
     def save_tree(self, tree):
         shelf = shelve.open(self.shelf_file.as_posix())
         shelf['tree'] = tree
         shelf.close()
+
     def load_tree(self):
         shelf = shelve.open(self.shelf_file.as_posix())
         if 'tree' in shelf:
             return shelf['tree']
         else:
             return None
+
     def load_info(self):
         shelf = shelve.open(self.shelf_file.as_posix())
         if 'info' in shelf:
             return shelf['info']
         else:
             return None
+
     def load_text(self, id):
         shelf = shelve.open(self.shelf_file.as_posix())
         if 'text' in shelf:
             return shelf['text']
         else:
             return None
+
     def search(self, phrase): pass
